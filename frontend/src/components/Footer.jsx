031de0bb2cee11d2d474e6a7a6eb66b6feb2119d
Shivansh <167235611+shivans-75@users.noreply.github.com>
2024-09-01 14:34:45 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="right">
+            <p>Malaviya Nagar, Jaipur</p>
+            <p>Open: 07:00 AM  - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>STARBUCKS</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
